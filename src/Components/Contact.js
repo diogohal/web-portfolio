40f81548f06ee71c66a0eebdc3c0cc3e315e829d
dha21 <dha21@inf.ufpr.br>
2023-03-16 14:41:59 -0300

    responsivity testing

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,10 +5,10 @@ export default function Contact() {
         <div className='contact' id='contact'>
             <h1 className='contact--title topic'>CONTATO</h1>
             <p className='contact--text description'>
-                Você pode me encontrar facilmente em diversas plataformas da internet, inclusive no 
+                Você pode me encontrar em diversas plataformas da internet, inclusive no 
                 <a className='contact--text--item' href='https://www.instagram.com/diogohal/'> Instagram</a>, 
                 <a className='contact--text--item' href='https://www.github.com/diogohal/'> GitHub</a>, 
-                <a className='contact--text--item' href='https://www.linkedin.com/in/diogohal/'> Linkedin</a>, 
+                <a className='contact--text--item' href='https://www.linkedin.com/in/diogohal/'> Linkedin </a>
                 ou através do meu 
                 <a className='contact--text--item' href="mailto: diogobr.2014@outlook.com"> email </a> 
                 diogobr.2014@outlook.com.
